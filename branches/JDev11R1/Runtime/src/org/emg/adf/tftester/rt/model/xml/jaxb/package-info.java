5cea2b7e91f6a8bba3fb214549f6eed65bf2fed1
wvanderdeijl <wvanderdeijl@56ece7c1-cf97-63a4-8744-db49cf97498b>
2013-01-04 08:47:11 +0000

    replace JAXB v1 with JAXB v2.1 that is included with JDK

---
+++ branches/JDev11R1/Runtime/src/org/emg/adf/tftester/rt/model/xml/jaxb/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://adf.emg.org/tftester", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.emg.adf.tftester.rt.model.xml.jaxb;
